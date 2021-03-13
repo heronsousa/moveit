6a9ce97c762c82d71061e613c3805e426c319b48
Heron Rodrigues <heronrs1@gmail.com>
2021-03-13 20:56:01 -0300

    Create countdown context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,26 @@ import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <ExperienceBar />
 
-            <section>
-                <div>
-                    <Profile />
-                    <CompletedChallenges />
-                    <Countdown />
-                </div>
-                <div>
-                    <ChallengeBox />
-                </div>
-            </section>
+            <CountdownProvider>
+                <section>
+                    <div>
+                        <Profile />
+                        <CompletedChallenges />
+                        <Countdown />
+
+                    </div>
+                    <div>
+                        <ChallengeBox />
+                    </div>
+                </section>
+            </CountdownProvider>
         </div>
     )
 }
